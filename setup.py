89fdb26a4142e8098654eb6765d264f837008706
Sunny Lam <sunny.lam@gmail.com>
2018-04-05 09:56:18 -0400

    revised setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
 	'description': 'The program makes checking weather easier by skipping the step of opening your web browser.  It downloads the weather forecast for the next few days and prints it as plain text.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
+	'url': 'https://github.com/sunnylam13/fetch_weather_data_040518_1',
+	'download_url': 'https://github.com/sunnylam13/fetch_weather_data_040518_1',
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
