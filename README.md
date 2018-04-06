6ad79eee059c5f7e3b9225fbef478e054d240d22
Sunny Lam <sunny.lam@gmail.com>
2018-04-06 16:20:32 -0400

    revised code and notes

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ The program makes checking weather easier by skipping the step of opening your w
 
 ## Execution
 
-	python3 fetch_weather_data_040518_1.py LOCATION
+	python3 fetch_weather_data_040518_1.py CITYNAME
 
 the test execution is:
 
-	python3 fetch_weather_data_040518_1.py "Toronto, ON"
-
-	python3 fetch_weather_data_040518_1.py "Toronto,ON"
-
 	python3 fetch_weather_data_040518_1.py Toronto
+
+### Using Country
+
+if you want to add the country then you have to use country codes under [ISO 3166 country codes](https://www.iso.org/iso-3166-country-codes.html)...
+
+	python3 fetch_weather_data_040518_1.py CITYNAME,COUNTRYCODE
+
+the test execution is:
+
+	python3 fetch_weather_data_040518_1.py Toronto, CA
 
 ## Resources
 
@@ -21,4 +27,6 @@ ABSP:  552
 [OpenWeatherMap.org](http://openweathermap.org/)
 
 [OpenWeatherMap API Key](http://openweathermap.org/appid)
+
+[ISO 3166 country codes](https://www.iso.org/iso-3166-country-codes.html)
 
--- fetch_weather_data/fetch_weather_data_040518_1.py
+++ fetch_weather_data/fetch_weather_data_040518_1.py
@@ -3,9 +3,11 @@
 #! python3
 
 # USAGE
-# python3 fetch_weather_data_040518_1.py
+# python3 fetch_weather_data_040518_1.py CITYNAME
 
 import json,requests,sys
+
+import pprint # optional import, only to see data to configure correct object, list key calls
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
@@ -41,12 +43,17 @@ logging.debug("API key is:  %s" % (api_key) )
 api_addition = "&APPID=" + api_key
 logging.debug("API addition to URL is:  %s" % (api_addition) )
 
-url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s' % (location)
+url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s%s' % (location,api_addition)
 logging.debug("URL to use is:  %s" % (url) )
 
 response = requests.get(url) # returns a response object
+logging.debug("Response object is:  ")
+logging.debug(response)
 logging.debug("Response status is:  ")
 logging.debug(response.raise_for_status()) # check status
+
+# logging.debug("Show response text:  ")
+# logging.debug(response.text) # shows the actual JSON data downloaded
 
 #####################################
 # END GET DATA
@@ -58,6 +65,16 @@ logging.debug(response.raise_for_status()) # check status
 #####################################
 
 # load JSON data into a Python variable
+
+weatherData = json.loads(response.text)
+
+# optional - display to screen in an easy to read way
+# requires `import pprint`
+# pprint.pprint(weatherData)
+
+# print weather descriptions
+
+w = weatherData['list']
 
 #####################################
 # END DATA PROCESSING
